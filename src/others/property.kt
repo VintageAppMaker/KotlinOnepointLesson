9522ecc84c4881149f11fbc4ef9df8267cccf387
snake <adsloader99@gmail.com>
2018-10-06 22:00:54 +0900

    ..

--- src/others/property.kt
+++ src/others/property.kt
@@ -18,8 +18,6 @@ var name : String = "박모씨"
 // 기존 클래스에 filed를 추가할 수 없지만
 // Properites는 추가가능하다.
 // 그러나 확장함수보다는 활용성이 무척적다.
-// 그렇게 할 바에야 class 설계할 때, Properites를
-// 구현하면되기 때문이다.
 
 class EmptyClass {
     var message : String = ""
@@ -30,16 +28,27 @@ var EmptyClass?.newProp : String
         }
         set(value) { this!!.message = value + "-" + this!!.message}
 
+// MutableList에 first 프로퍼티 추가
+var MutableList<String>?.first : String
+    get() {return this!!.get(0)}
+    set(value) {this!!.add(0, value)}
 
 fun main(args : Array<String> ){
     name = "동작구 에미넴 #2"
     println (name)
 
+    // 내가만든 클래스에 확장 프로퍼티 추가
     val empty = EmptyClass()
     empty.newProp = "안녕"
     empty.newProp = "반가와요"
     empty.newProp = "새로운 클래스"
-
     println (empty.newProp)
+
+    // mutableList에 확장 프로퍼티 추가
+    val mList = mutableListOf<String>("나는", "코틀린", "입니다")
+    println(mList)
+    mList.first = "너는"
+    println(mList)
+    println(mList.first)
 
 }
