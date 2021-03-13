b94d51c7988617f24b24630e5d32293f8b3f80d5
vintageappmaker <adsloader99@gmail.com>
2021-03-13 10:50:57 +0900

    gradle 적용

--- src/main/kotlin/asynclib/asynctest.kt
+++ src/main/kotlin/asynclib/asynctest.kt
@@ -8,18 +8,22 @@ import org.jsoup.Jsoup
 
 // Android에서는 runBlocking(점유율)과 GlobalScope(전역적)는 사용하지 않는 것이 좋다.
 fun main() = runBlocking{
-    // 1, 2의 동시 작업을 통해 imgList를 처리
-    var imgList = mutableListOf<String>()
+    // 1, 2에서 동시 작업을 통해 imgList와 종료정보를 공유함
+    var imgList   = mutableListOf<String>()
+    var bComplete : Boolean = false;
+    var onComplete :() -> Unit  = { bComplete = true}
 
     // 1. 네트웍을 통해 정보가져오기(imgList 저장)
-    getImageInfoFromNetwork("http://vintageappmaker.com", imgList)
+    getImageInfoFromNetwork("http://vintageappmaker.com", imgList, onComplete)
 
     // 2. UI 작업(imgList 보여주기)
     launch {
-        (0..10).forEach {
+        (0..100).forEach {
             println (">> Another UI Working.. $it")
             println (">> imgList count =>  ${imgList.count()}")
-            delay(1000)
+            delay(500)
+
+            if(bComplete) cancel()
         }
     }
 
@@ -27,7 +31,7 @@ fun main() = runBlocking{
     delay(1000 * 10)
 }
 
-fun getImageInfoFromNetwork(url : String, imgList : MutableList<String>) {
+fun getImageInfoFromNetwork(url : String, imgList : MutableList<String>, onComplete : () -> Unit) {
     CoroutineScope(Dispatchers.IO).launch {
         flow{
             var doc = Jsoup.connect(url).get()
@@ -37,9 +41,17 @@ fun getImageInfoFromNetwork(url : String, imgList : MutableList<String>) {
                 emit(sUrl)
                 delay(400)
             }
+
+            // 모두 처리했음
+            emit(true)
         }.collect {
-            println ("image download complete => $it")
-            imgList.add(it)
+            if (it is String ) {
+                println("image download complete => $it")
+                imgList.add(it)
+            } else {
+                // 종료 callback 호출
+                onComplete()
+            }
         }
     }
 }
